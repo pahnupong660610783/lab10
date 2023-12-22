3ed740462076fa6aaed88e1be2e066a0909f3391
pahnupong660610783 <pahnupong_kaeopramun@cmu.ac.th>
2023-12-22 14:06:14 +0700

    lab10

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    
+    int all=0;
+    double sum = 0, xi = 0;
+    string text;
+    
+	while(getline(source,text)){
+	     sum += atof(text.c_str());
+	     xi += pow(atof(text.c_str()),2);
+	     all++;
+	}
+	
+    cout << "Number of data = " << all << "\n";
+    cout << setprecision(3);
+    double mean = sum/all;
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << sqrt((xi/all)-pow(mean,2));
+    
+    
+    
+    
+    
+    
+    
+    
+}
